185454a7335d6b18dc86f9722bd25bf17eef605f
ryoma okuda <catontheskate4423@gmail.com>
2021-06-14 12:21:33 +0900

    connected to mongodb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, ParseBoolPipe, Body } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, ParseIntPipe } from '@nestjs/common';
 // import { EADDRNOTAVAIL } from 'constants';
 import { AppService } from './app.service';
 
@@ -8,14 +8,13 @@ export class AppController {
 
   @Post(':id')
   async insertNum(
-    @Param() params
+    @Param('id', new ParseIntPipe()) id
   ) {
-    return this.appService.insertNum(params.id)
+    return this.appService.insertNum(id)
   }
 
   @Get()
   async getArray(
-    // @Body('sort', ParseBoolPipe) sort: boolean,
   ) {
     return this.appService.showAll(true)
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb+srv://ryoma:${process.env.MONGODB_PASSWORD}@cluster0.evsnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
